41500e8bed446af82aae6335d44e52aac142f64d
brainandbodycode <brainandbodycode@users.noreply.github.com>
2022-07-24 21:43:49 +0000

    deploy: a53b0b4d998bd7b29239c9cb5354535992e2bd8b

---
+++ _nuxt/ProseH4-3419b87c.mjs
@@ -0,0 +1,1 @@
+import{A as n,o as s,b as a,J as r,w as c,r as d,f as _}from"./entry-295dd7ce.mjs";const i=["id"],u=n({__name:"ProseH4",props:{id:null},setup(e){return(t,l)=>{const o=_;return s(),a("h4",{id:e.id},[r(o,{href:`#${e.id}`},{default:c(()=>[d(t.$slots,"default")]),_:3},8,["href"])],8,i)}}});export{u as default};
--- _nuxt/index-97a07985.mjs
+++
@@ -1,1 +0,0 @@
-import{a as n,o,b as t,J as c,M as _}from"./entry-a30ec2c9.mjs";const a={};function r(s,d){const e=_;return o(),t("div",null,[c(e,{path:"/home"})])}var p=n(a,[["render",r]]);export{p as default};
