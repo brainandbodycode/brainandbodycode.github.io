ca86579077a2da9cce979c8f90f23b33f1744189
brainandbodycode <brainandbodydev@gmail.com>
2022-07-24 00:02:26 -0700

    Update booking.vue

--- components/content/booking.vue
+++ components/content/booking.vue
@@ -1,8 +1,8 @@
 <template>
     <!-- <iframe v-frame="'https://app.squarespacescheduling.com/schedule.php?owner=26788133&appointmentType=35501038'"
         title="Schedule Appointment" width="100%" height="100%" frameBorder="0"></iframe> -->
-    <div ref="booking">
-        <div id="intakeq" style="width: 100%;"></div>
+    <div v-intake>
+        <div id="intakeq"></div>
     </div>
 </template>
 
@@ -11,25 +11,24 @@
 import iframeResize from 'iframe-resizer/js/iframeResizer';
 
 export default {
-    // directives: {
-    //     frame: {
-    //         mounted(el, src) {
-    //             // el.addEventListener('load', () => iframeResize({}, el))
-    //             iframeResize({ log: true }, el);
-    //             el.setAttribute("src", src.value);
-    //         },
-    //         unmounted(el) {
-    //             console.log(el.iFrameResizer)
-    //             el.iFrameResizer.removeListeners();
-    //         }
-    //     }
-    // },
-    mounted() {
-        window.intakeq = "62dcb6cada9e6433106f200a";
-        var i = document.createElement("script");
-        i.type = "text/javascript";
-        i.async = true; i.src = "https://intakeq.com/js/widget.min.js?1";
-        this.$refs.booking.appendChild(i);
-    }
+    directives: {
+        intake: {
+            mounted(el) {
+                window.intakeq = "62dcb6cada9e6433106f200a";
+                var i = document.createElement("script");
+                i.type = "text/javascript";
+                i.async = true;
+                i.src = "https://intakeq.com/js/widget.min.js?1";
+                el.appendChild(i);
+            },
+            unmounted(el) {
+                window.intakeq = undefined;
+                const iframe = el.querySelector('iframe');
+                const script = el.querySelector('script');
+                iframe.remove();
+                script.remove();
+            }
+        }
+    },
 }
 </script>
