bdb212c0d65123ec8c7aeda03e4ed9b609148994
jeremy <brainandbodydev@gmail.com>
2022-07-30 22:57:51 -0700

    submit form

--- components/content/contact-form.vue
+++ components/content/contact-form.vue
@@ -1,34 +1,42 @@
 <template>
     <div class="grid">
         <div class="contact-form">
-            <h2 class="contact-heading">Lets start our conversation</h2>
-            <form class="contact-form-form" @submit.prevent="onSubmit">
+            <h2 v-if="!submitted" class="contact-heading">Lets start our conversation</h2>
+            <div v-if="submitted">
+                <h2>Thank you for you submission.</h2>
+                <p>We will get back to you shortly.</p>
+            </div>
+            <form v-else ref="form" class="contact-form-form" @submit.prevent="onSubmit">
                 <label for="fname">
                     <span>First Name</span>
-                    <input type="text" name="fname" placeholder="First name">
+                    <input type="text" name="fname" placeholder="First name" required>
                 </label>
                 <label for="lname">
                     <span>Last Name</span>
-                    <input type="text" name="lname" placeholder="Last name">
+                    <input type="text" name="lname" placeholder="Last name" required>
                 </label>
                 <label for="email">
                     <span>Email</span>
-                    <input type="email" name="email" placeholder="Email">
+                    <input type="email" name="email" placeholder="Email" required>
                 </label>
                 <label for="subject">
                     <span>Subject</span>
-                    <input type="text" name="subject" placeholder="Subject">
+                    <input type="text" name="subject" placeholder="Subject" required>
                 </label>
                 <label for="travel">
                     <span>What area will we be traveling to?</span>
                     <input type="text" name="travel"
-                        placeholder="Downtown Spokane, Spokane Valley, Deer Park, Liberty Lake">
+                        placeholder="Downtown Spokane, Spokane Valley, Deer Park, Liberty Lake" required>
                 </label>
                 <label for="message">
                     <span>Message</span>
-                    <textarea name="message" cols="30" rows="10"
+                    <textarea required name="message" cols="30" rows="10"
                         placeholder="I would like more information on how to relieve tension, help my kiddo, receive an in-home massage…"></textarea>
                 </label>
+                <button type="submit" :disabled="loading">
+                    <span v-if="loading">Sending Message</span>
+                    <span v-else>Send Message</span>
+                </button>
             </form>
         </div>
     </div>
@@ -36,9 +44,27 @@
 
 <script>
 export default {
+    data() {
+        return {
+            loading: false,
+            submitted: false,
+        }
+    },
     methods: {
-        onSubmit() {
-            console.log('submit')
+        async onSubmit() {
+            this.loading = true;
+            const formElem = this.$refs.form;
+            let response = await fetch('https://script.google.com/macros/s/AKfycbwayz36om-CC08N2L3gMvb8aFx2q7j75VCIT4MgRbOnkPSITC8UCNKiC0EotFF6WNJa/exec', {
+                method: 'POST',
+                body: new FormData(formElem)
+            });
+
+            let result = await response.json();
+            if (result.result === 'success') {
+                this.submitted = true;
+            } else {
+                this.loading = false;
+            }
         }
     }
 }
@@ -66,6 +92,10 @@ export default {
         textarea {
             width: 100%;
             box-sizing: border-box;
+        }
+
+        button {
+            background-color: var(--color4);
         }
     }
 }
---
+++ components/content/loader.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="lds-ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</template>
+
+<style>
+.lds-ring {
+    display: inline-block;
+    position: relative;
+    width: 16px;
+    height: 16px;
+    justify-content: center;
+    align-items: center;
+}
+
+.lds-ring div {
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: 16px;
+    height: 16px;
+    top: 1px;
+    left: 1px;
+    border: 1px solid var(--black);
+    border-radius: 50%;
+    animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: var(--black) transparent transparent transparent;
+}
+
+.lds-ring div:nth-child(1) {
+    animation-delay: -0.45s;
+}
+
+.lds-ring div:nth-child(2) {
+    animation-delay: -0.3s;
+}
+
+.lds-ring div:nth-child(3) {
+    animation-delay: -0.15s;
+}
+
+@keyframes lds-ring {
+    0% {
+        transform: rotate(0deg);
+    }
+
+    100% {
+        transform: rotate(360deg);
+    }
+}
+</style>
