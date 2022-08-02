a886ed43ebfa28433bc3b1ba86b2e62cc54f2193
jeremy <brainandbodydev@gmail.com>
2022-08-01 21:30:11 -0700

    Update vue-gtag.client.js

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -3,7 +3,7 @@ import VueGtag from 'vue-gtag'
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(VueGtag, {
         config: {
-            id: 'G-SBV6KDBWNF',
+            id: 'G-LX5Q6642L7',
         },
     }, nuxtApp.$router)
 })
