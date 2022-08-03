473c311d75f9c598631d8035b2fc0a14e834ee6a
jeremy <brainandbodydev@gmail.com>
2022-08-02 21:15:16 -0700

    301s

---
+++ pages/book-now1.vue
@@ -0,0 +1,7 @@
+<template>
+    <div></div>
+</template>
+
+<script setup>
+navigateTo('/services-and-rates', { redirectCode: 301 })
+</script>
---
+++ pages/learn-more.vue
@@ -0,0 +1,7 @@
+<template>
+    <div></div>
+</template>
+
+<script setup>
+navigateTo('/massage-types', { redirectCode: 301 })
+</script>
