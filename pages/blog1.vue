f7724319f09373001b7c0bcd13620d9a5723722d
jeremy <brainandbodydev@gmail.com>
2022-08-02 21:10:03 -0700

    redirects

---
+++ pages/blog1.vue
@@ -0,0 +1,7 @@
+<template>
+    <div></div>
+</template>
+
+<script setup>
+navigateTo('/blog', { redirectCode: 301 })
+</script>
---
+++ pages/contact1.vue
@@ -0,0 +1,7 @@
+<template>
+    <div></div>
+</template>
+
+<script setup>
+navigateTo('/', { redirectCode: 301 })
+</script>
