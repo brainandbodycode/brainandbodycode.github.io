183ad0047c21763bd23c61682f46ab5a51786cb5
jeremy <brainandbodydev@gmail.com>
2022-08-02 21:05:42 -0700

    redirects

---
+++ pages/bio.vue
@@ -0,0 +1,7 @@
+<template>
+    <div></div>
+</template>
+
+<script setup>
+navigateTo('/about-us')
+</script>
---
+++ pages/mnri.vue
@@ -0,0 +1,7 @@
+<template>
+    <div></div>
+</template>
+
+<script setup>
+navigateTo('/blogs/mnri')
+</script>
